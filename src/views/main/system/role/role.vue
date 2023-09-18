64cd141b6846d09be94c863e8dd780a747778d20
Tawnyi <649645654@qq.com>
2023-09-18 23:57:44 +0800

    feat(main): 首页页面和路由对象的创建

---
+++ src/views/main/system/role/role.vue
@@ -0,0 +1,19 @@
+<!-- 角色管理 -->
+<template>
+  <div class="role">
+    <h2>role</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'role',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ src/views/main/system/user/user.vue
@@ -0,0 +1,19 @@
+<!-- 用户管理 -->
+<template>
+  <div class="user">
+    <h2>user</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'user',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
