9416cb8622142cad14e904d583374e0db5965506
Tawnyi <649645654@qq.com>
2023-09-22 17:01:07 +0800

    feat(form): 用户页Form初步封装

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -6,17 +6,25 @@
     <el-icon v-else class="fold-menu" @click="handleFoldClick">
       <Expand />
     </el-icon>
+    <div class="content">
+      <div>面包屑</div>
+      <div class="user-info">
+        <user-info />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { Fold, Expand } from '@element-plus/icons-vue'
+import UserInfo from './user-info.vue'
 
 export default defineComponent({
   components: {
     Fold,
-    Expand
+    Expand,
+    UserInfo
   },
   emits: ['foldChange'],
   setup(props, { emit }) {
@@ -35,9 +43,18 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
   .fold-menu {
     font-size: 30px;
     cursor: pointer; // 放上去时，显示小手
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,17 +1,86 @@
 <!-- 用户管理 -->
 <template>
   <div class="user">
-    <h2>user</h2>
+    <div class="search">
+      <hy-form
+        :formItems="formItems"
+        :labelWidth="labelWidth"
+        :itemStyle="itemStyle"
+        :collLayout="collLayout"
+      />
+    </div>
+    <div class="content"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HyForm, { IFormItem } from '@/base-ui/form'
 
 export default defineComponent({
   name: 'user',
+  components: {
+    HyForm
+  },
   setup() {
-    return {}
+    const formItems: IFormItem[] = [
+      {
+        type: 'input',
+        label: 'id',
+        rules: [],
+        placeholder: '请输入id'
+      },
+      {
+        type: 'input',
+        label: '用户名',
+        rules: [],
+        placeholder: '请输入用户名'
+      },
+      {
+        type: 'password',
+        label: '密码',
+        rules: [],
+        placeholder: '请输入密码'
+      },
+      {
+        type: 'select',
+        label: '喜欢的运动',
+        rules: [],
+        placeholder: '请选择喜欢的运动',
+        options: [
+          { label: '篮球', value: 'basketball' },
+          { label: '足球', value: 'football' }
+        ]
+      },
+      {
+        type: 'datepicker',
+        label: '创建时间',
+        rules: [],
+        placeholder: '请选择创建时间范围',
+        otherOptions: {
+          startPlaceholder: '开始时间',
+          endPlaceholder: '结束时间',
+          type: 'daterange'
+        }
+      }
+    ]
+
+    const labelWidth = '120px'
+
+    const itemStyle = {
+      padding: '10px 30px'
+    }
+
+    const collLayout = {
+      span: 8
+    }
+
+    return {
+      formItems,
+      labelWidth,
+      itemStyle,
+      collLayout
+    }
   }
 })
 </script>
