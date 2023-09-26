d83f2fa68b7e575e163176628400d2c55e434190
Tawnyi <649645654@qq.com>
2023-09-26 19:59:16 +0800

    feat(user-table): elTable展示数据鹤Hytable封装

---
+++ src/base-ui/table/src/table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hy-table">
+    <el-table :data="listData" border style="width: 100%">
+      <template v-for="propItem in propList" :key="propItem.prop">
+        <el-table-column
+          :prop="propItem.props"
+          :label="propItem.label"
+          :min-width="propItem.minWidth"
+          align="center"
+        />
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    listData: {
+      type: Array,
+      required: true
+    },
+    propList: {
+      type: Array,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,7 +2,9 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
-    <div class="content"></div>
+    <div class="content">
+      <hy-table :listData="userList" :propList="propList"></hy-table>
+    </div>
   </div>
 </template>
 
@@ -13,11 +15,13 @@ import { useStore } from '@/store'
 import { searchFormConfig } from './config/search.config'
 
 import PageSearch from '@/components/page-search'
+import HyTable from '@/base-ui/table'
 
 export default defineComponent({
   name: 'user',
   components: {
-    PageSearch
+    PageSearch,
+    HyTable
   },
   setup() {
     const store = useStore()
@@ -32,11 +36,28 @@ export default defineComponent({
     const userList = computed(() => store.state.system.useList)
     const userCount = computed(() => store.state.system.userCount)
 
+    const propList = [
+      { props: 'name', label: '用户名', minWidth: '100' },
+      { props: 'realname', label: '真实姓名', minWidth: '100' },
+      { props: 'cellphone', label: '电话号码', minWidth: '100' },
+      { props: 'enable', label: '状态', minWidth: '100' },
+      { props: 'createAt', label: '创建时间', minWidth: '250' },
+      { props: 'updateAt', label: '更新时间', minWidth: '250' }
+    ]
+
     return {
-      searchFormConfig
+      searchFormConfig,
+      userList,
+      userCount,
+      propList
     }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
