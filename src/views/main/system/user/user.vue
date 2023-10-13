1a6c177c18721e601bcee92515427a8ba937c75e
Tawnyi <649645654@qq.com>
2023-10-14 00:34:37 +0800

    feat(user、department): 刷新页面部门和角色动态数据展示

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -17,12 +17,13 @@
   <page-modal
     :defaultInfo="defaultInfo"
     ref="pageModalRef"
-    :modalConfig="modalConfig"
+    :modalConfig="modalConfigRef"
   ></page-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '@/store'
 
 import { searchFormConfig } from './config/search.config'
 import { contentTableConfig } from './config/content.config'
@@ -46,6 +47,7 @@ export default defineComponent({
     const [pageContentRef, handleResetClick, handleQueryClick] = usePageSearch()
 
     // pageModal相关的hook逻辑
+    // 1.处理密码的逻辑
     const newCallback = () => {
       const passwordItem = modalConfig.formItems.find(
         (item) => item.field === 'password'
@@ -58,6 +60,26 @@ export default defineComponent({
       )
       passwordItem!.isHidden = true
     }
+
+    // 2.动态添加部门和角色列表
+    const store = useStore()
+    const modalConfigRef = computed(() => {
+      const departmentItem = modalConfig.formItems.find(
+        (item) => item.field === 'departmentId'
+      )
+      departmentItem!.options = store.state.entireDepartment.map((item) => {
+        return { label: item.name, value: item.id }
+      })
+      const roleItem = modalConfig.formItems.find(
+        (item) => item.field === 'roleId'
+      )
+      roleItem!.options = store.state.entireRole.map((item) => {
+        return { label: item.name, value: item.id }
+      })
+      return modalConfig
+    })
+
+    // 3.调用hook获取公共变量和函数
     const [pageModalRef, defaultInfo, handleNewData, handleEditData] =
       usePageModal(newCallback, editCallback)
 
@@ -67,7 +89,7 @@ export default defineComponent({
       pageContentRef,
       handleResetClick,
       handleQueryClick,
-      modalConfig,
+      modalConfigRef,
       handleNewData,
       handleEditData,
       pageModalRef,
