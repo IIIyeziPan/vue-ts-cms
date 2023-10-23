53867bc95818011ed2f51d31237efba551bc5038
Tawnyi <649645654@qq.com>
2023-10-23 22:10:01 +0800

    feat(base-echart): baseEchart封装和setup使用

---
+++ src/base-ui/echart/src/base-echart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="base-echar">
+    <div ref="echartDivRef" :style="{ width: width, height: height }"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, defineProps, withDefaults } from 'vue'
+import * as echarts from 'echarts'
+import { EchartsOption } from 'echarts'
+
+// 定义props
+const props = withDefaults(
+  defineProps<{
+    options: EchartsOption
+    width?: string
+    height?: string
+  }>(),
+  {
+    width: '100%',
+    height: '360px'
+  }
+)
+
+const echartDivRef = ref<HTMLElement>()
+
+onMounted(() => {
+  const echartInstance = echarts.init(echartDivRef.value!)
+  echartInstance.setOption(props.options)
+})
+</script>
+
+<style lang="less" scoped></style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -14,7 +14,9 @@
 
     <el-row :gutter="10" class="content-row">
       <el-col :span="12">
-        <hy-card title="分类商品的销量"></hy-card>
+        <hy-card title="分类商品的销量">
+          <base-echart :options="options"></base-echart>
+        </hy-card>
       </el-col>
       <el-col :span="12">
         <hy-card title="分类商品的收藏)"></hy-card>
@@ -28,16 +30,41 @@ import { defineComponent } from 'vue'
 import { useStore } from 'vuex'
 
 import HyCard from '@/base-ui/card'
+import BaseEchart from '@/base-ui/echart'
 
 export default defineComponent({
   name: 'dashboard',
   components: {
-    HyCard
+    HyCard,
+    BaseEchart
   },
   setup() {
     const store = useStore()
     store.dispatch('dashboard/getDashboardDataAction')
-    return {}
+
+    const options = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar',
+          showBackground: true,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)'
+          }
+        }
+      ]
+    }
+
+    return {
+      options
+    }
   }
 })
 </script>
